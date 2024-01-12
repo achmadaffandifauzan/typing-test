55cd77623194b6035d218941f9c1ed38fc472398
achmadaffandifauzan <achmadaffandifauzan@gmail.com>
2024-01-12 23:49:42 +0700

    auth: update header display

--- app/header.tsx
+++ app/header.tsx
@@ -8,25 +8,39 @@ const Header = () => {
   const getAuthBtn = () => {
     if (session) {
       return (
-        <div className="flex flex-row px-7">
-          <div className="flex flex-col justify-center items-start">
-            <div>Hi {session?.user?.name?.split(" ")[0]}! </div>
-            <button className="px-2 " onClick={() => signOut()}>
-              Logout
-            </button>
-          </div>
-          <img className="sm:w-9 w-5" src="/icons/waving-hand.svg" alt="" />
-        </div>
+        <button
+          className="bg-indigo-100 hover:bg-indigo-500 hover:text-white text-indigo-500 font-semibold flex flex-row sm:w-full justify-center items-center text-sm h-10 sm:py-2 py-0.5 px-3 rounded-b-xl rounded-r-none hover:ring-4 hover:shadow-xl transition-all"
+          onClick={() => signOut()}
+        >
+          <div>Logout</div>
+          <img src="/icons/logout.svg" className="sm:w-6 w-5" alt="" />
+        </button>
       );
     } else {
       return (
         <button
-          className="bg-indigo-100 hover:bg-indigo-500 hover:text-white text-indigo-500 font-semibold flex flex-row items-center text-sm h-10 sm:py-2 py-0.5 px-3 sm:rounded-b-xl rounded-r-none hover:ring-4 hover:shadow-xl transition-all"
+          className="bg-indigo-100 hover:bg-indigo-500 hover:text-white text-indigo-500 font-semibold flex flex-row items-center text-sm h-10 sm:py-2 py-0.5 px-3 sm:rounded-b-xl max-sm:rounded-r-none max-sm:rounded-l-xl hover:ring-4 hover:shadow-xl transition-all"
           onClick={() => signIn()}
         >
           <img src="/icons/login.svg" className="sm:w-6 w-5" alt="" />
           <div>Login to save result!</div>
         </button>
+      );
+    }
+  };
+  const displayGreeting = () => {
+    if (session) {
+      return (
+        <div className="absolute sm:top-5 sm:right-5 right-28 flex flex-row justify-center items-center max-sm:text-center ">
+          <div>
+            Hi{" "}
+            <span className="font-bold">
+              {session?.user?.name?.split(" ")[0]}
+            </span>
+            !
+          </div>
+          <img src="/icons/waving-hand.svg" className="sm:w-7 w-5" alt="" />
+        </div>
       );
     }
   };
@@ -39,6 +53,7 @@ const Header = () => {
         </Link>
         {getAuthBtn()}
       </div>
+      {displayGreeting()}
     </div>
   );
 };
