ed77f1971b9d20af5f482dea05402a003ce6ea1b
achmadaffandifauzan <achmadaffandifauzan@gmail.com>
2024-01-12 07:28:30 +0700

    update footer

--- app/page.tsx
+++ app/page.tsx
@@ -474,7 +474,7 @@ const Home = () => {
               {openNotif ? (
                 <span className="text-yellow-500 font-bold">RANDOM</span>
               ) : (
-                <span className="text-red-500 font-bold">RANDOM</span>
+                <span className="text-blue-500 font-bold">RANDOM</span>
               )}
               , generated from
               <a
