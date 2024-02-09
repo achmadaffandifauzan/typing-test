554cabb68675eade30ef1c8ffb40c5dbea00fe8c
achmadaffandifauzan <achmadaffandifauzan@gmail.com>
2024-02-09 21:44:07 +0700

    remove custom session declaration

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -1,9 +1,9 @@
-import NextAuth, { DefaultSession } from "next-auth";
-// custom to add username in session next auth
-declare module "next-auth" {
-  interface Session {
-    user: {
-      username: string;
-    } & DefaultSession["user"];
-  }
-}
+// import NextAuth, { DefaultSession } from "next-auth";
+// // custom to add username in session next auth
+// declare module "next-auth" {
+//   interface Session {
+//     user: {
+//       username: string;
+//     } & DefaultSession["user"];
+//   }
+// }
