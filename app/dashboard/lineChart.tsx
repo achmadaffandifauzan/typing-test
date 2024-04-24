e4ff0733936353e7d90cdd0b7ea535966fb4ef0b
achmadaffandifauzan <achmadaffandifauzan@gmail.com>
2024-04-24 20:22:15 +0700

    add chart to dashboard page

--- app/dashboard/lineChart.tsx
+++ app/dashboard/lineChart.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import {
   Chart as ChartJS,
@@ -10,6 +11,7 @@ import {
   Legend,
 } from "chart.js";
 import { Line } from "react-chartjs-2";
+
 ChartJS.register(
   CategoryScale,
   LinearScale,
@@ -27,31 +29,18 @@ export const options = {
     },
     title: {
       display: true,
-      text: "Chart.js Line Chart",
+      text: "Your Past Results",
     },
   },
+  maintainAspectRatio: true,
 };
-const labels = ["January", "February", "March", "April", "May", "June", "July"];
-
-export const data = {
-  labels,
-  datasets: [
-    {
-      label: "Dataset 1",
-      // data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-      borderColor: "rgb(255, 99, 132)",
-      backgroundColor: "rgba(255, 99, 132, 0.5)",
-    },
-    {
-      label: "Dataset 2",
-      // data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
-      borderColor: "rgb(53, 162, 235)",
-      backgroundColor: "rgba(53, 162, 235, 0.5)",
-    },
-  ],
-};
-const lineChart = () => {
-  return <div>chart</div>;
+const LineChart = ({ accuracyChartData, wpmChartData, labels }: any) => {
+  const data = {
+    labels,
+    datasets: [accuracyChartData, wpmChartData],
+  };
+  console.log(data);
+  return <Line options={options} data={data} />;
 };
 
-export default lineChart;
+export default LineChart;
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,21 +4,49 @@ import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { getOneUser } from "@/prisma/functions/user";
 import dayjs from "dayjs";
-
+import LineChart from "./lineChart";
+import { getTypingHistories } from "@/prisma/functions/typing";
 const Dashboard = async () => {
   const session = await getServerSession(authOptions);
   const user = await getOneUser(session?.user.username!);
   const userDateCreation = dayjs(user?.createdAt!.toString()).format(
     "MMM D, YYYY"
   );
+  const typingHistories = await getTypingHistories(user?.username);
+  // console.log("typingHistories", typingHistories);
+  // change typing format to be chart data
+
+  const labels: any = typingHistories?.map((typing) => {
+    return dayjs(typing.createdAt.toString()).format("D/MM/YY");
+  });
+  const accuracyChartData: any = {
+    label: "Accuracy",
+    data: typingHistories?.map((typing) => {
+      return typing.accuracy;
+    }),
+    borderColor: "#37a345",
+    backgroundColor: "#37a345",
+  };
+  const wpmChartData: any = {
+    label: "WPM",
+    data: typingHistories?.map((typing) => {
+      return typing.wpm;
+    }),
+    borderColor: "#d1221f",
+    backgroundColor: "#d1221f",
+  };
 
   return (
-    <div className="w-full sm:h-screen flex flex-col">
+    <div className="w-full min-h-screen flex flex-col">
       <Header />
-      <div className="flex flex-col sm:h-screen px-10">
-        <div className="flex flex-row h-4/6 flex-wrap justify-between items-center sm:pt-20">
-          <div className="w-3/12 bg-indigo-100 font-semibold text-indigo-500 px-4 py-3 rounded-xl text-center">
-            <div>Name : {user?.name}</div>
+      <div className="flex flex-col px-5">
+        <div className="flex sm:flex-row flex-col flex-wrap justify-around items-center sm:pt-20 ">
+          <div className="sm:w-3/12 bg-indigo-100 font-semibold text-indigo-500 px-4 py-3 rounded-xl text-center">
+            {user?.name && (
+              <div>
+                <div>Name : {user?.name}</div>
+              </div>
+            )}
             {session?.user.email && (
               <div>
                 Email : <span>{session?.user.email}</span>
@@ -33,9 +61,14 @@ const Dashboard = async () => {
               User since {userDateCreation}
             </div>
           </div>
-          <div className="w-8/12 bg-slate-100">Graph</div>
+          <div className="sm:w-6/12 bg-indigo-100 rounded-xl sm:p-4 p-1 flex justify-start items-center">
+            <LineChart
+              accuracyChartData={accuracyChartData}
+              wpmChartData={wpmChartData}
+              labels={labels}
+            ></LineChart>
+          </div>
         </div>
-        <div className="flex h-2/6 bg-slate-50">Keyboard</div>
       </div>
     </div>
   );
